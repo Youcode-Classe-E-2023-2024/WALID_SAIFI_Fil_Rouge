5552daa484074d0b77f9889d945c8b9c0d4ccb98
Walid Saifi <saifiwalid21@gmail.com>
2024-04-05 11:20:58 +0000

    Create home.blade.php

---
+++ fill_rouge/resources/views/home.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Page Homme - Ville Safi</title>
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+</head>
+<body>
+<nav>
+    <ul>
+        <li><a href="#">Homme</a></li>
+        <li><a href="#">Mes Marternaires</a></li>
+        <li><a href="#">Mes Vendeurs</a></li>
+        <li><a href="#">Mes Livreurs</a></li>
+        <li><a href="#">Ville Safi</a></li>
+    </ul>
+</nav>
+
+</body>
+</html>
