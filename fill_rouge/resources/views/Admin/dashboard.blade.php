d4cb99fde17111d09f0734249d5f3836554c0fbf
Walid Saifi <saifiwalid21@gmail.com>
2024-04-07 23:27:16 +0000

    Update dashboard.blade.php

--- fill_rouge/resources/views/Admin/dashboard.blade.php
+++ fill_rouge/resources/views/Admin/dashboard.blade.php
@@ -7,16 +7,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Majestic Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css">
-    <link rel="stylesheet" href="vendors/base/vendor.bundle.base.css">
+    <link rel="stylesheet" href="{{ asset('dashbord/vendors/mdi/css/materialdesignicons.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('dashbord/vendors/base/vendor.bundle.base.css') }}">
+
     <!-- endinject -->
     <!-- plugin css for this page -->
-    <link rel="stylesheet" href="vendors/datatables.net-bs4/dataTables.bootstrap4.css">
+    <link rel="stylesheet" href="{{ asset('dashbord/vendors/datatables.net-bs4/dataTables.bootstrap4.css')}}">
     <!-- End plugin css for this page -->
     <!-- inject:css -->
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ asset('dashbord/css/style.css')}}">
     <!-- endinject -->
-    <link rel="shortcut icon" href="images/favicon.png" />
+    <link rel="shortcut icon" href="{{ asset('dashbord/images/favicon.png')}}" />
 </head>
 <body>
 <div class="container-scroller">
@@ -404,90 +405,70 @@
                 <div class="row">
                     <div class="col-md-12 stretch-card">
                         <div class="card">
-                            <div class="card-body">
-                                <p class="card-title">Recent Purchases</p>
-                                <div class="table-responsive">
-                                    <table id="recent-purchases-listing" class="table">
-                                        <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Status report</th>
-                                            <th>Office</th>
-                                            <th>Price</th>
-                                            <th>Date</th>
-                                            <th>Gross amount</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                        <tr>
-                                            <td>Jeremy Ortega</td>
-                                            <td>Levelled up</td>
-                                            <td>Catalinaborough</td>
-                                            <td>$790</td>
-                                            <td>06 Jan 2018</td>
-                                            <td>$2274253</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Alvin Fisher</td>
-                                            <td>Ui design completed</td>
-                                            <td>East Mayra</td>
-                                            <td>$23230</td>
-                                            <td>18 Jul 2018</td>
-                                            <td>$83127</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Emily Cunningham</td>
-                                            <td>support</td>
-                                            <td>Makennaton</td>
-                                            <td>$939</td>
-                                            <td>16 Jul 2018</td>
-                                            <td>$29177</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Minnie Farmer</td>
-                                            <td>support</td>
-                                            <td>Agustinaborough</td>
-                                            <td>$30</td>
-                                            <td>30 Apr 2018</td>
-                                            <td>$44617</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Betty Hunt</td>
-                                            <td>Ui design not completed</td>
-                                            <td>Lake Sandrafort</td>
-                                            <td>$571</td>
-                                            <td>25 Jun 2018</td>
-                                            <td>$78952</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Myrtie Lambert</td>
-                                            <td>Ui design completed</td>
-                                            <td>Cassinbury</td>
-                                            <td>$36</td>
-                                            <td>05 Nov 2018</td>
-                                            <td>$36422</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Jacob Kennedy</td>
-                                            <td>New project</td>
-                                            <td>Cletaborough</td>
-                                            <td>$314</td>
-                                            <td>12 Jul 2018</td>
-                                            <td>$34167</td>
-                                        </tr>
-                                        <tr>
-                                            <td>Ernest Wade</td>
-                                            <td>Levelled up</td>
-                                            <td>West Fidelmouth</td>
-                                            <td>$484</td>
-                                            <td>08 Sep 2018</td>
-                                            <td>$50862</td>
-                                        </tr>
-                                        </tbody>
-                                    </table>
+
+
+
+
+
+                            <div class="content-wrapper">
+                                <div class="row">
+                                    <div class="col-lg-6 grid-margin stretch-card">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Line chart</h4>
+                                                <canvas id="lineChart"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 grid-margin stretch-card">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Bar chart</h4>
+                                                <canvas id="barChart"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6 grid-margin stretch-card">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Area chart</h4>
+                                                <canvas id="areaChart"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 grid-margin stretch-card">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Doughnut chart</h4>
+                                                <canvas id="doughnutChart"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="col-lg-6 grid-margin grid-margin-lg-0 stretch-card">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Pie chart</h4>
+                                                <canvas id="pieChart"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 grid-margin grid-margin-lg-0 stretch-card">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Scatter chart</h4>
+                                                <canvas id="scatterChart"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
+
+
+
                     </div>
                 </div>
             </div>
@@ -508,26 +489,37 @@
 <!-- container-scroller -->
 
 <!-- plugins:js -->
-<script src="vendors/base/vendor.bundle.base.js"></script>
+<script src="{{ asset('dashbord/vendors/base/vendor.bundle.base.js')}}"></script>
 <!-- endinject -->
 <!-- Plugin js for this page-->
-<script src="vendors/chart.js/Chart.min.js"></script>
-<script src="vendors/datatables.net/jquery.dataTables.js"></script>
-<script src="vendors/datatables.net-bs4/dataTables.bootstrap4.js"></script>
+<script src="{{ asset('dashbord/vendors/chart.js/Chart.min.js')}}"></script>
+<script src="{{ asset('dashbord/vendors/datatables.net/jquery.dataTables.js')}}"></script>
+<script src="{{ asset('dashbord/vendors/datatables.net-bs4/dataTables.bootstrap4.js')}}"></script>
 <!-- End plugin js for this page-->
 <!-- inject:js -->
-<script src="js/off-canvas.js"></script>
-<script src="js/hoverable-collapse.js"></script>
-<script src="js/template.js"></script>
+<script src="{{ asset('dashbord/js/off-canvas.js')}}"></script>
+<script src="{{ asset('dashbord/js/hoverable-collapse.js')}}"></script>
+<script src="{{ asset('dashbord/js/template.js')}}"></script>
 <!-- endinject -->
 <!-- Custom js for this page-->
-<script src="js/dashboard.js"></script>
-<script src="js/data-table.js"></script>
-<script src="js/jquery.dataTables.js"></script>
-<script src="js/dataTables.bootstrap4.js"></script>
-<!-- End custom js for this page-->
+<script src="{{ asset('dashbord/js/dashboard.js')}}"></script>
+<script src="{{ asset('dashbord/js/data-table.js')}}"></script>
+<script src="{{ asset('dashbord/js/jquery.dataTables.js')}}"></script>
+<script src="{{ asset('dashbord/js/dataTables.bootstrap4.js')}}"></script>
 
-<script src="js/jquery.cookie.js" type="text/javascript"></script>
+    <!-- endinject -->
+    <!-- Plugin js for this page-->
+    <script src="{{ asset('dashbord/vendors/chart.js/Chart.min.js')}}"></script>
+    <!-- End plugin js for this page-->
+    <!-- inject:js -->
+    <script src="{{ asset('dashbord/js/off-canvas.js')}}"></script>
+    <script src=".{{ asset('dashbord/js/hoverable-collapse.js')}}"></script>
+    <script src="{{ asset('dashbord/js/template.js')}}"></script>
+    <!-- endinject -->
+    <!-- Custom js for this page-->
+    <script src="{{ asset('dashbord/js/chart.js')}}"></script>
+<!-- End custom js for this page-->
+<script src="{{ asset('dashbord/js/jquery.cookie.js')}}" type="text/javascript"></script>
 </body>
 
 </html>
