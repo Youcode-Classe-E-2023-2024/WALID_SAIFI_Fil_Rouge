cfb933977c7cef4413c2e1a1cd4009ec41f5de66
Walid Saifi <saifiwalid21@gmail.com>
2024-04-08 15:43:29 +0000

    Update AuthController.php

--- fill_rouge/app/Http/Controllers/AuthController.php
+++ fill_rouge/app/Http/Controllers/AuthController.php
@@ -8,7 +8,10 @@ use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-
+    public function showRegistrationForm()
+    {
+        return view('sinup');
+    }
 
     public function register(Request $request)
     {
@@ -16,13 +19,20 @@ class AuthController extends Controller
             'name' => 'required|string',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:8|confirmed',
+            'role' => 'required|integer', // Assurez-vous que le champ de rôle est requis et de type entier
         ]);
 
-        User::create([
+        $user = User::create([
             'name' => $request->name,
             'email' => $request->email,
             'password' => Hash::make($request->password),
         ]);
+
+        // Récupérez le rôle sélectionné
+        $role = Role::find($request->role);
+
+        // Attachez le rôle à l'utilisateur
+        $user->roles()->attach($role);
 
         return redirect('/login')->with('success', 'Votre compte a été créé avec succès ! Vous pouvez vous connecter maintenant.');
     }
