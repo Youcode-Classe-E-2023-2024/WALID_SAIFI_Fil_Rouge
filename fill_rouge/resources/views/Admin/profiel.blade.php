efce5c84cf6d04a4474e619a78712d9689e26e7e
Walid Saifi <saifiwalid21@gmail.com>
2024-04-08 10:35:17 +0000

    Update profiel.blade.php

--- fill_rouge/resources/views/Admin/profiel.blade.php
+++ fill_rouge/resources/views/Admin/profiel.blade.php
@@ -1,12 +1,9 @@
 @extends('Admin.layout')
 
 @section('content')
-
-    <div class="card-body">
+    <div class="card" style="width: 700px;">
+    <div class="card-body" >
         <h4 class="card-title">Votre Profil</h4>
-        <p class="card-description">
-            Mise en page de formulaire basique
-        </p>
         <form class="forms-sample">
             <div class="form-group">
                 <label for="exampleInputUsername1">Nom d'utilisateur</label>
@@ -25,16 +22,23 @@
                 <input type="password" class="form-control" id="exampleInputConfirmPassword1" placeholder="Mot de passe">
             </div>
 
-            <div class="input-group col-xs-12">
-                <input type="text" class="form-control file-upload-info" disabled placeholder="Télécharger une image">
-                <span class="input-group-append">
-                      <button class="file-upload-browse btn btn-primary" type="button">Télécharger votre image</button>
-                    </span>
+            <div class="form-group">
+                <label>File upload</label>
+                <input type="file" name="img[]" class="file-upload-default">
+                <div class="input-group col-xs-12">
+                    <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
+                    <span class="input-group-append">
+                          <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
+                        </span>
+                </div>
             </div>
+
 
             <button type="submit" class="btn btn-primary me-2">Soumettre</button>
             <button class="btn btn-light">Annuler</button>
         </form>
     </div>
+    </div>
+
 
 @endsection
