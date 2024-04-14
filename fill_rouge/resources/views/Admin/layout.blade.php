356b552fe2ee225f8bf4e0da9b232ee8118632dd
Walid Saifi <saifiwalid21@gmail.com>
2024-04-14 19:14:49 +0100

    Update layout.blade.php

--- fill_rouge/resources/views/Admin/layout.blade.php
+++ fill_rouge/resources/views/Admin/layout.blade.php
@@ -225,11 +225,7 @@
 
                 <!-- content-wrapper ends -->
                 <!-- partial:partials/_footer.html -->
-                <footer class="footer">
-                    <div class="d-sm-flex justify-content-center justify-content-sm-between">
-                        <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright ©  2024</span>
-                    </div>
-                </footer>
+
                 <!-- partial -->
             </div>
             <!-- main-panel ends -->
