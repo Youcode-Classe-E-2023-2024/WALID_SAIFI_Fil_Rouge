df85ffb5ece70e3478d999d626723c40cc462eb5
Walid Saifi <saifiwalid21@gmail.com>
2024-04-14 16:04:01 +0100

    Update 2014_10_12_000000_create_users_table.php

--- fill_rouge/database/migrations/2014_10_12_000000_create_users_table.php
+++ fill_rouge/database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,7 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->string('image')->nullable();
+            $table->boolean('validation')->default(0);
             $table->timestamps();
         });
     }
