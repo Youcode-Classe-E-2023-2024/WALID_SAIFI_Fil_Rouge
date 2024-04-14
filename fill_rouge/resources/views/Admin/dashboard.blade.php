dc57ccd74b3a244ecccabbbdf122b2a1c75bf457
Walid Saifi <saifiwalid21@gmail.com>
2024-04-14 17:10:01 +0100

    Update dashboard.blade.php

--- fill_rouge/resources/views/Admin/dashboard.blade.php
+++ fill_rouge/resources/views/Admin/dashboard.blade.php
@@ -142,17 +142,23 @@
                 <li class="nav-item nav-profile dropdown">
                     <a class="nav-link dropdown-toggle" href="#" data-bs-toggle="dropdown" id="profileDropdown">
                         <img src="images/faces/face5.jpg" alt="profile"/>
-                        <span class="nav-profile-name">Louis Barnett</span>
+                        <span class="nav-profile-name">Admin : {{ Auth::user()->name }}</span>
+
                     </a>
                     <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="profileDropdown">
                         <a class="dropdown-item">
                             <i class="mdi mdi-settings text-primary"></i>
                             Settings
                         </a>
-                        <a class="dropdown-item">
-                            <i class="mdi mdi-logout text-primary"></i>
-                            Logout
-                        </a>
+                        <form action="{{ route('user.deconnecter') }}" method="POST">
+                            @csrf
+                            <button type="submit" class="dropdown-item">
+                                <i class="mdi mdi-logout text-primary"></i>
+                                Déconnexion
+                            </button>
+                        </form>
+
+
                     </div>
                 </li>
             </ul>
