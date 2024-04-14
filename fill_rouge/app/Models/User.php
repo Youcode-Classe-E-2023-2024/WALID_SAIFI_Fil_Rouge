1a90813c4c77c0aa5e7857a4f30b38ec291bbe83
Walid Saifi <saifiwalid21@gmail.com>
2024-04-14 16:29:56 +0100

    Update User.php

--- fill_rouge/app/Models/User.php
+++ fill_rouge/app/Models/User.php
@@ -2,7 +2,6 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,33 +11,29 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-
     public function roles()
     {
         return $this->belongsToMany(Role::class, 'users_roles');
     }
 
-
     public function isAdmin()
     {
-        return $this->roles()->where('id', 1)->exists();
+        return $this->roles()->where('role_id', 1)->exists();
     }
 
     public function isVendor()
     {
-        return $this->roles()->where('id', 2)->exists();
+        return $this->roles()->where('role_id', 2)->exists();
     }
 
     public function isUser()
     {
-        return $this->roles()->where('id', 3)->exists();
+        return $this->roles()->where('role_id', 3)->exists();
     }
-
 }
