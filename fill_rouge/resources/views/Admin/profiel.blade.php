3333bdb73bb58732ff18e09e4a16c3eec9b594b4
Walid Saifi <saifiwalid21@gmail.com>
2024-04-15 09:47:04 +0100

    Update profiel.blade.php

--- fill_rouge/resources/views/Admin/profiel.blade.php
+++ fill_rouge/resources/views/Admin/profiel.blade.php
@@ -1,44 +1,51 @@
 @extends('Admin.layout')
 
 @section('content')
+
+    @if(session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+
     <div class="card" style="width: 700px;">
-    <div class="card-body" >
-        <h4 class="card-title">Votre Profil</h4>
-        <form class="forms-sample">
-            <div class="form-group">
-                <label for="exampleInputUsername1">Nom d'utilisateur</label>
-                <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Nom d'utilisateur">
-            </div>
-            <div class="form-group">
-                <label for="exampleInputEmail1">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-            </div>
-            <div class="form-group">
-                <label for="exampleInputPassword1">Mot de passe</label>
-                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Mot de passe">
-            </div>
-            <div class="form-group">
-                <label for="exampleInputConfirmPassword1">Confirmer le mot de passe</label>
-                <input type="password" class="form-control" id="exampleInputConfirmPassword1" placeholder="Mot de passe">
-            </div>
-
-            <div class="form-group">
-                <label>File upload</label>
-                <input type="file" name="img[]" class="file-upload-default">
-                <div class="input-group col-xs-12">
-                    <input type="text" class="form-control file-upload-info" disabled placeholder="Upload Image">
-                    <span class="input-group-append">
-                          <button class="file-upload-browse btn btn-primary" type="button">Upload</button>
-                        </span>
+        <div class="card-body">
+            <h4 class="card-title">Modifier Votre Profil</h4>
+            <form method="POST" action="{{ route('profile.update') }}" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="form-group">
+                    <label for="exampleInputUsername1">Nom</label>
+                    <input type="text" class="form-control" id="exampleInputUsername1" name="name" placeholder="Nom" value="{{ old('name') ?: auth()->user()->name }}">
                 </div>
-            </div>
-
-
-            <button type="submit" class="btn btn-primary me-2">Soumettre</button>
-            <button class="btn btn-light">Annuler</button>
-        </form>
+                <div class="form-group">
+                    <label for="exampleInputUsername1">Prénom</label>
+                    <input type="text" class="form-control" id="exampleInputUsername1" name="prenom" placeholder="Prénom" value="{{ old('prenom') ?: auth()->user()->prenom }}">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Email</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" name="email" placeholder="Email" value="{{ auth()->user()->email }}">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputPassword1">Mot de passe</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1" name="password" placeholder="Mot de passe">
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputConfirmPassword1">Confirmer le mot de passe</label>
+                    <input type="password" class="form-control" id="exampleInputConfirmPassword1" name="password_confirmation" placeholder="Mot de passe">
+                </div>
+                <div class="form-group">
+                    <label>Ajouter Votre image</label>
+                    <input type="file" name="img" class="form-control-file">
+                </div>
+                <button type="submit" class="btn btn-primary me-2">Soumettre</button>
+            </form>
+            @if(auth()->user()->img)
+                <div class="mt-3">
+                    <p>Image actuelle:</p>
+                    <img src="{{ asset('images/'.auth()->user()->img) }}" alt="Votre image" style="max-width: 200px;">
+                </div>
+            @endif
+        </div>
     </div>
-    </div>
-
-
 @endsection
