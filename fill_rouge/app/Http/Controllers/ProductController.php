41b146a3aeacc5bb7f81b1b6881a1b7e1d05fc3d
Walid Saifi <saifiwalid21@gmail.com>
2024-04-15 16:28:57 +0100

    Update ProductController.php

--- fill_rouge/app/Http/Controllers/ProductController.php
+++ fill_rouge/app/Http/Controllers/ProductController.php
@@ -6,35 +6,38 @@ use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
+
 class ProductController extends Controller
 {
 
+public  function index(){
+    return view('vendeur.dashVendeur');
+}
     public function indexAjouterProduit() {
         $categories = Category::all();
-        dd($categories);
-        return view('ajouterProduit', compact('categories'));
+        return view('vendeur.ajouterProduit', compact('categories'));
     }
 
 
     public function ajouterProduit(Request $request)
     {
-        // Validation des données
+
         $request->validate([
             'titre' => 'required|string|max:255',
             'prix' => 'required|numeric',
             'categorie' => 'required|integer',
             'img' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'description' => 'nullable|string', // La description est facultative
+            'description' => 'nullable|string',
         ]);
 
         $produit = new Product();
         $produit->titre = $request->titre;
         $produit->prix = $request->prix;
-        $produit->categorie_id = $request->id_categorie;
+        $produit->id_categorie = $request->id_categorie;
         $produit->description = $request->description;
 
 
-        // Gérer le téléchargement de l'image si une nouvelle image est fournie
+
         if ($request->file('img')) {
             $file = $request->file('img');
             @unlink(public_path('/images/Produit' .  $produit ->image)); // delete previous photo
@@ -43,7 +46,7 @@ class ProductController extends Controller
             $user['image'] = $filename;
         }
         $produit->save();
-        // Redirection avec un message de succès
+
         return redirect()->back()->with('success', 'Produit ajouté avec succès');
     }
 
