b95bc8623f57944d94a60f88f412ec0975774ba5
Walid Saifi <saifiwalid21@gmail.com>
2024-04-15 14:35:23 +0100

    Update Product.php

--- fill_rouge/app/Models/Product.php
+++ fill_rouge/app/Models/Product.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'titre', 'prix', 'id_categorie', 'images','description'
+    ];
+
+
+    // Relation "appartient à" avec la catégorie
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'id_categorie');
+    }
+
 }
