bdaebcf4d6921e183ebe854033c97cf66f0c8896
Walid Saifi <saifiwalid21@gmail.com>
2024-04-15 15:09:08 +0100

    Update ProductController.php

--- fill_rouge/app/Http/Controllers/ProductController.php
+++ fill_rouge/app/Http/Controllers/ProductController.php
@@ -2,11 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+
+    public function indexAjouterProduit() {
+        $categories = Category::all();
+        dd($categories);
+        return view('ajouterProduit', compact('categories'));
+    }
+
+
     public function ajouterProduit(Request $request)
     {
         // Validation des données
@@ -14,25 +23,26 @@ class ProductController extends Controller
             'titre' => 'required|string|max:255',
             'prix' => 'required|numeric',
             'categorie' => 'required|integer',
-            'images.*' => 'image|mimes:jpeg,png,jpg,gif,svg|max:2048', // Validation des images (optionnel)
+            'img' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
             'description' => 'nullable|string', // La description est facultative
         ]);
 
         $produit = new Product();
         $produit->titre = $request->titre;
         $produit->prix = $request->prix;
-        $produit->categorie_id = $request->categorie;
+        $produit->categorie_id = $request->id_categorie;
         $produit->description = $request->description;
-        $produit->save();
 
-        // Gestion des images (s'il y en a)
-        if ($request->hasFile('images')) {
-            foreach ($request->file('images') as $image) {
-                $path = $image->store('chemin/vers/dossier'); // Spécifiez le chemin où vous souhaitez enregistrer l'image
-                $produit->images()->create(['chemin' => $path]);
-            }
+
+        // Gérer le téléchargement de l'image si une nouvelle image est fournie
+        if ($request->file('img')) {
+            $file = $request->file('img');
+            @unlink(public_path('/images/Produit' .  $produit ->image)); // delete previous photo
+            $filename = date('YmdHi') . $file->getClientOriginalName();
+            $file->move(public_path('/images/Produit'), $filename);
+            $user['image'] = $filename;
         }
-
+        $produit->save();
         // Redirection avec un message de succès
         return redirect()->back()->with('success', 'Produit ajouté avec succès');
     }
