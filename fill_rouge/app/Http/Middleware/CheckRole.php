320d5412101e4585c7f3ae1bcd5c7a9d05bab18e
Walid Saifi <saifiwalid21@gmail.com>
2024-04-16 11:36:52 +0100

    Update CheckRole.php

--- fill_rouge/app/Http/Middleware/CheckRole.php
+++ fill_rouge/app/Http/Middleware/CheckRole.php
@@ -18,16 +18,15 @@ class CheckRole
      */
     public function handle(Request $request, Closure $next)
     {
-        // Vérifier si l'utilisateur est authentifié
+
         if (Auth::check()) {
-            // Vérifier si l'utilisateur a le rôle spécifié
-            if (Auth::user()->isAdmin()) { // Utilisation de la méthode isAdmin
-                // L'utilisateur a le rôle d'administrateur, donc autoriser la demande
+
+            if (Auth::user()->isAdmin()) {
+
                 return $next($request);
             }
         }
 
-        // Rediriger ou renvoyer une réponse en cas d'échec de l'autorisation
         return redirect()->route('login')->with('error', 'Vous n\'êtes pas autorisé à accéder à cette ressource.');
     }
 }
