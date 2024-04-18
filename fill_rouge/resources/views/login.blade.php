8a3cf9c58f5bce817d7af82e7fb6a94b65b838aa
Walid Saifi <saifiwalid21@gmail.com>
2024-04-18 14:16:59 +0100

    Update login.blade.php

--- fill_rouge/resources/views/login.blade.php
+++ fill_rouge/resources/views/login.blade.php
@@ -26,7 +26,7 @@
                 <div class="col-lg-6 d-flex align-items-center justify-content-center">
                     <div class="auth-form-transparent text-left p-3">
                         <div class="brand-logo">
-                            <img src="{{ asset('dashbord/images/logo.svg') }}" alt="logo">
+                            <img src="{{ asset('dashbord/images/shopabda.png') }}" alt="logo">
                         </div>
                         @if(session('success'))
                             <div class="alert alert-success">
