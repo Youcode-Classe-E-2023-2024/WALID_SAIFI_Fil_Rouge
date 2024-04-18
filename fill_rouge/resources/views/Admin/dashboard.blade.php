62a5d91471750d966e09cb3ea15b752558b1c097
Walid Saifi <saifiwalid21@gmail.com>
2024-04-18 12:13:29 +0100

    Update dashboard.blade.php

--- fill_rouge/resources/views/Admin/dashboard.blade.php
+++ fill_rouge/resources/views/Admin/dashboard.blade.php
@@ -63,13 +63,28 @@
 
 
 
+                            <div class="content-wrapper">
+                                <div class="row">
+                                    <div class="col-lg-6 grid-margin stretch-card">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Bar chart</h4>
+                                                <canvas id="barChart"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 grid-margin grid-margin-lg-0 stretch-card">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h4 class="card-title">Pie chart</h4>
+                                                <canvas id="pieChart"></canvas>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
 
-
-
-
-
-
-
+                                    </div>
+                            
                     </div>
                 </div>
             </div>
