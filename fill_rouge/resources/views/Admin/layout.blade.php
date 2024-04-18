673cbdd796f6bf0a3d290784695ccf8e18d0a284
Walid Saifi <saifiwalid21@gmail.com>
2024-04-18 21:32:23 +0100

    Update layout.blade.php

--- fill_rouge/resources/views/Admin/layout.blade.php
+++ fill_rouge/resources/views/Admin/layout.blade.php
@@ -35,50 +35,13 @@
             </div>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
-            <ul class="navbar-nav mr-lg-4 w-100">
-                <li class="nav-item nav-search d-none d-lg-block w-100">
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                <span class="input-group-text" id="search">
-                  <i class="mdi mdi-magnify"></i>
-                </span>
-                        </div>
-                        <input type="text" class="form-control" placeholder="Search now" aria-label="search" aria-describedby="search">
-                    </div>
-                </li>
-            </ul>
+
             <ul class="navbar-nav navbar-nav-right">
                 <li class="nav-item dropdown me-1">
                     <a class="nav-link count-indicator dropdown-toggle d-flex justify-content-center align-items-center" id="messageDropdown" href="#" data-bs-toggle="dropdown">
-                        <i class="mdi mdi-message-text mx-0"></i>
-                        <span class="count"></span>
+
                     </a>
                     <div class="dropdown-menu dropdown-menu-right navbar-dropdown" aria-labelledby="messageDropdown">
-                        <p class="mb-0 font-weight-normal float-left dropdown-header">Messages</p>
-                        <a class="dropdown-item">
-                            <div class="item-thumbnail">
-                                <img src="{{ asset('dashbord/images/faces/face4.jpg')}}" alt="image" class="profile-pic">
-                            </div>
-                            <div class="item-content flex-grow">
-                                <h6 class="ellipsis font-weight-normal">David Grey
-                                </h6>
-                                <p class="font-weight-light small-text text-muted mb-0">
-                                    The meeting is cancelled
-                                </p>
-                            </div>
-                        </a>
-                        <a class="dropdown-item">
-                            <div class="item-thumbnail">
-                                <img src="{{ asset('dashbord/images/faces/face2.jpg')}}" alt="image" class="profile-pic">
-                            </div>
-                            <div class="item-content flex-grow">
-                                <h6 class="ellipsis font-weight-normal">Tim Cook
-                                </h6>
-                                <p class="font-weight-light small-text text-muted mb-0">
-                                    New product launch
-                                </p>
-                            </div>
-                        </a>
                         <a class="dropdown-item">
                             <div class="item-thumbnail">
                                 <img src="{{ asset('dashbord/images/faces/face3.jpg')}}" alt="image" class="profile-pic">
