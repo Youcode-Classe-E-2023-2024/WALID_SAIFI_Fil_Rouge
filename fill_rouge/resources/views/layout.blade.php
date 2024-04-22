87640f511e754424103a6ba05e9288495eed7ad9
Walid Saifi <saifiwalid21@gmail.com>
2024-04-22 09:34:45 +0100

    Update layout.blade.php

--- fill_rouge/resources/views/layout.blade.php
+++ fill_rouge/resources/views/layout.blade.php
@@ -19,6 +19,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
 
     <!-- Vendor CSS Files -->
+    <link rel="stylesheet" href="{{asset('css_desc/bootstrap.min.css')}}" />
+    <link rel="stylesheet" href="{{asset('css_desc/animate.css')}}" />
+    <link rel="stylesheet" href="{{asset('css_desc/lineicons.css')}}" />
+    <link rel="stylesheet" href="{{asset('css_desc/ud-styles.css')}}" />
+
     <link href="{{asset('frontend/assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">
     <link href="{{asset('frontend/assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">
     <link href="{{asset('frontend/assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
