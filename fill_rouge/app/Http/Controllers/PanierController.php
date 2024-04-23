be7720f703135e4660fb0c214600b06e5d778e0d
Walid Saifi <saifiwalid21@gmail.com>
2024-04-23 09:30:27 +0100

    Create PanierController.php

---
+++ fill_rouge/app/Http/Controllers/PanierController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Models\Panier;
+use App\Models\Product;
+
+class PanierController extends Controller
+{
+
+    public function ajouterProduit(Request $request)
+    {
+
+        $request->validate([
+            'product_id' => 'required|exists:products,id',
+            'quantite' => 'required|integer|min:1',
+
+        ]);
+
+
+        $userId = Auth::id();
+
+
+        $productId = $request->input('product_id');
+        $quantite = $request->input('quantite');
+
+        $product = Product::findOrFail($productId);
+
+
+        $prixTotal = $product->prix * $quantite;
+
+
+        $panier = new Panier();
+        $panier->product_id = $productId;
+        $panier->user_id = $userId;
+        $panier->quantite = $quantite;
+        $panier->prix_total = $prixTotal;
+
+
+        $panier->save();
+
+
+        return redirect()->back()->with('success', 'Produit ajouté au panier avec succès!');
+    }
+
+}
