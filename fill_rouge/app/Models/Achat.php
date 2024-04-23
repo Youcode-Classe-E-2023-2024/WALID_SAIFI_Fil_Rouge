d5b268ea2531e57468199097b4273f18c39b3ef8
Walid Saifi <saifiwalid21@gmail.com>
2024-04-23 14:41:03 +0100

    Create Achat.php

---
+++ fill_rouge/app/Models/Achat.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Achat extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'adresse',
+        'num_telephone',
+        'prix_total'
+    ];
+
+
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function product()
+    {
+        return $this->belongsTo(Product::class);
+    }
+
+
+}
