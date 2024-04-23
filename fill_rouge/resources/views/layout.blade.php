93bbc72766f44dfb131734776d3dd89a8d6f0c30
Walid Saifi <saifiwalid21@gmail.com>
2024-04-23 12:03:52 +0100

    Update layout.blade.php

--- fill_rouge/resources/views/layout.blade.php
+++ fill_rouge/resources/views/layout.blade.php
@@ -66,23 +66,7 @@
                 <li><a href="{{route('home')}}" class="active">Home</a></li>
                 <li><a href="{{ url('/') }}#about">About</a></li>
                 <li><a href="{{ url('/') }}#portfolio">Portfolio</a></li>
-                <li class="dropdown has-dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down"></i></a>
-                    <ul class="dd-box-shadow">
-                        <li><a href="#">Dropdown 1</a></li>
-                        <li class="dropdown has-dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down"></i></a>
-                            <ul class="dd-box-shadow">
-                                <li><a href="#">Deep Dropdown 1</a></li>
-                                <li><a href="#">Deep Dropdown 2</a></li>
-                                <li><a href="#">Deep Dropdown 3</a></li>
-                                <li><a href="#">Deep Dropdown 4</a></li>
-                                <li><a href="#">Deep Dropdown 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Dropdown 2</a></li>
-                        <li><a href="#">Dropdown 3</a></li>
-                        <li><a href="#">Dropdown 4</a></li>
-                    </ul>
-                </li>
+
                 <li><a href="{{ url('/') }}#contact">Contact</a></li>
                 <!-- Augmentation de la taille de l'icône de panier -->
                 <li>
@@ -96,7 +80,7 @@
                                     <!-- Icône de corbeille Bootstrap avec taille augmentée -->
                                     <i class="bi bi-trash text-danger fs-5 me-2 delete-product" data-product-id="{{ $item->product->id }}"></i>
                                     <!-- Titre du produit avec la classe de couleur de texte Bootstrap -->
-                                    <span class="text-dark">{{ $item->product->titre }}</span>
+                                    <span class="text-dark">{{ $item->product->titre }} X {{$item->quantite}}</span>
                                 </li>
                             @endforeach
                             <li><button class="btn btn-primary ms-auto">Acheter</button></li>
