06bdf7906f7567c3c121ce9640223e874d83f7d0
Walid Saifi <saifiwalid21@gmail.com>
2024-04-24 16:56:11 +0100

    Update home1.blade.php

--- fill_rouge/resources/views/home1.blade.php
+++ fill_rouge/resources/views/home1.blade.php
@@ -55,24 +55,9 @@
             <ul>
                 <li><a href="{{route('home')}}" class="active">Home</a></li>
                 <li><a href="{{ url('/') }}#about">About</a></li>
-                <li><a href="">Espace-produit</a></li>
-                <li class="dropdown has-dropdown"><a href="#"><span>Dropdown</span> <i class="bi bi-chevron-down"></i></a>
-                    <ul class="dd-box-shadow">
-                        <li><a href="#">Dropdown 1</a></li>
-                        <li class="dropdown has-dropdown"><a href="#"><span>Deep Dropdown</span> <i class="bi bi-chevron-down"></i></a>
-                            <ul class="dd-box-shadow">
-                                <li><a href="#">Deep Dropdown 1</a></li>
-                                <li><a href="#">Deep Dropdown 2</a></li>
-                                <li><a href="#">Deep Dropdown 3</a></li>
-                                <li><a href="#">Deep Dropdown 4</a></li>
-                                <li><a href="#">Deep Dropdown 5</a></li>
-                            </ul>
-                        </li>
-                        <li><a href="#">Dropdown 2</a></li>
-                        <li><a href="#">Dropdown 3</a></li>
-                        <li><a href="#">Dropdown 4</a></li>
-                    </ul>
-                </li>
+                <li><a href="{{route('index.produit')}}">Espace-produit</a></li>
+
+
                 <li><a href="{{ url('/') }}#contact">Contact</a></li>
 
 
